98e2ae5f4e38ba28d9f7a148ff9ba79b2fd7b729
mzacom <oyetolamuiz78@gmail.com>
2023-01-01 12:49:34 -0800

    bugs fixed

--- src/components/SevicesH.jsx
+++ src/components/SevicesH.jsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "../style";
+import { useState } from "react";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -11,42 +12,49 @@ import { RxArrowTopRight } from "react-icons/rx";
 import { ServiceData } from "../constants";
 
 const ServicesH = () => {
+  const [over, setOver] = useState(false);
+
   return (
+    <div
+      className={`flex items-center justify-center py-[2rem] flex-col sm:h-[650px] h-[500px] ${styles.greyBg} `}
+    >
+      <h1 className=" text-xl mb-[1rem] font-bold  ">Our Service</h1>
 
-
-    <div className={`flex items-center justify-center flex-col sm:h-[650px] h-[400px] ${styles.greyBg} `}>
-
-      <h1 className=" text-xl mb-[1rem] font-bold " >Our Service</h1>
       <Swiper
         breakpoints={{
           340: {
-            slidesPerView: 2,
+            slidesPerView: 1,
             spaceBetween: 15,
           },
           700: {
             slidesPerView: 3,
             spaceBetween: 15,
           },
+          
         }}
         freeMode={true}
         pagination={{
           clickable: true,
         }}
         modules={[FreeMode, Pagination]}
-        className="max-w-[90%] lg:max-w-[80%]"
+        className="max-w-[95%] rounded-xl lg:max-w-[80%]"
       >
         {ServiceData.map((item) => (
           <SwiperSlide key={item.title}>
-            <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
+            <div
+              className={`flex flex-col gap-7  mb-20 group   shadow-lg text-white rounded-xl px-6 py-8 h-[550px] w-[315px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer`}
+              onClick={() => setOver(!over)}
+            >
               <div
-                className="absolute inset-0 bg-cover bg-center"
+                className="absolute inset-0 rounded-xl  bg-cover bg-center"
                 style={{ backgroundImage: `url(${item.backgroundImage})` }}
               />
-              <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
-              <div className="relative flex flex-col gap-3">
+              <div className={`absolute inset-0 rounded-xl bg-black opacity-10 group-hover:opacity-50`} />
+
+              <div className="relative  flex flex-col gap-3">
                 <item.icon className="text-[lime] group-hover:text-[lime] w-[32px] h-[32px]" />
                 <h1 className="text-xl lg:text-2xl">{item.title} </h1>
-                <p className="lg:text-[18px]">{item.content} </p>
+                <p className="lg:text-[18px] text-[10px]  ">{item.content} </p>
               </div>
               <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-[lime] group-hover:rotate-45 duration-100" />
             </div>
