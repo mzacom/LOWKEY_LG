6434044fb2bf92b3c1efe72508c71ee7e868cd8b
mzacom <oyetolamuiz78@gmail.com>
2024-05-12 10:15:03 -0700

    bugs

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 import {
   NavBar,
   Hero,
@@ -16,22 +16,8 @@ import styles from "../../style";
 const Home = () => {
   const [showPage, setShowPage] = useState(false);
 
-  useEffect(() => {
-    // Check if the animations have already been shown
-    if (!localStorage.getItem('animationsShown')) {
-      // If not, show the animations
-      setShowPage(true);
-    } else {
-      // If they have been shown, don't show them again
-      setShowPage(false);
-    }
-  }, []);
-
   const handleAnimationComplete = () => {
-    // When the animations complete, set a flag in localStorage
-    localStorage.setItem('animationsShown', 'true');
-    // Optionally, you can force a re-render or navigate away to see the change
-    // For example, using a router to navigate to the same route would cause a re-render
+    setShowPage(true);
   };
 
   return (
@@ -47,4 +33,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Home
