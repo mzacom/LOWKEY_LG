43fdaf3de6b2ad6f741746d1efa94fd2e37e7182
mzacom <oyetolamuiz78@gmail.com>
2024-05-12 10:04:50 -0700

    bugs

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 import {
   NavBar,
   Hero,
@@ -16,25 +16,16 @@ import styles from "../../style";
 const Home = () => {
   const [showPage, setShowPage] = useState(false);
 
-  useEffect(() => {
-    // Check if the component has mounted before
-    if (!localStorage.getItem('homeMounted')) {
-      // Set a flag in localStorage indicating that the component has mounted
-      localStorage.setItem('homeMounted', 'true');
-      // Show the animations since this is the first load
-      setShowPage(true);
-    } else {
-      // If the component has mounted before, don't show the animations
-      setShowPage(true);
-    }
-  }, []);
+  const handleAnimationComplete = () => {
+    setShowPage(true);
+  };
 
   return (
     <>
       {/* Section A (Navbar and Hero section)  */}
       <section className={` ${styles.section}  `}>
-        {!showPage && <CarAnimation onComplete={() => setShowPage(true)} />}
-        {!showPage && <TextAnimation onComplete={() => setShowPage(true)} />}
+        {!showPage && <CarAnimation onComplete={handleAnimationComplete} />}
+        {!showPage && <TextAnimation onComplete={handleAnimationComplete} />}
         {showPage && <NavBar />}
         {showPage && <Hero />}
       </section>
@@ -42,4 +33,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Home
