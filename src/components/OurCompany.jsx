5239f8b58947aff47c37daa6ef3d1ef52f3ad4a3
mzacom <oyetolamuiz78@gmail.com>
2024-05-12 14:21:25 -0700

    bugs

--- src/components/OurCompany.jsx
+++ src/components/OurCompany.jsx
@@ -3,21 +3,18 @@ import styles from "../style";
 import { deliveryImg } from "../assets";
 import { motion } from "framer-motion";
 
-
-
 const OurCompany = () => {
   return (
     <>
       <motion.section
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 2 }}
+        transition={{ duration: 3 }}
         className={`relative flex flex-col sm:flex-row bg-cover bg-center w-full h-auto ${styles.padding} py-[2rem] ${styles.paddingX}`}
         style={{
-          backgroundImage: `linear-gradient(to right, rgba(0, 255, 0, 0.6), rgba(0, 255, 0, 0.6)), url(${deliveryImg})`
+          backgroundImage: `linear-gradient(to right, rgba(0, 255, 0, 0.6), rgba(0, 255, 0, 0.6)), url(${deliveryImg})`,
         }}
       >
-
         <section className="card w-full h-[440px]"></section>
       </motion.section>
     </>
