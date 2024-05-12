718c2831bfa968fb4cf51842eac712c81c7bd027
mzacom <oyetolamuiz78@gmail.com>
2024-05-12 14:37:53 -0700

    bugs fixed

--- src/components/OurCompany.jsx
+++ src/components/OurCompany.jsx
@@ -11,11 +11,13 @@ const OurCompany = () => {
       <section
         className={`relative flex flex-col sm:flex-row bg-cover bg-center w-full h-auto ${styles.padding} py-[2rem] ${styles.paddingX}`}
         style={{
-          backgroundImage: `linear-gradient(to right, rgba(0, 255, 0, 0.6), rgba(0, 255, 0, 0.6)), url(${deliveryImg})`
+          backgroundImage: `linear-gradient(to right, rgba(0, 255, 0, 0.292), rgba(0, 255, 0, 0.297)), url(${deliveryImg})`
         }}
       >
 
-        <section className="card w-full h-[440px]"></section>
+        <section className="card2 w-full h-[400px]">
+          
+        </section>
       </section>
     </>
   );
