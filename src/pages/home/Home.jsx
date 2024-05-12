9b29979a6bff4f18330feecc416fb9fc813d16a7
mzacom <oyetolamuiz78@gmail.com>
2024-05-12 11:58:27 -0700

    bugs yen po , but ko si gele 😂

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   NavBar,
   Hero,
@@ -7,30 +7,22 @@ import {
   Why,
   Get,
   Footer,
-  CarAnimation,
-  TextAnimation,
 } from "../../components";
 
 import styles from "../../style";
 
 const Home = () => {
-  const [showPage, setShowPage] = useState(false);
-
-  const handleAnimationComplete = () => {
-    setShowPage(true);
-  };
+  
 
   return (
     <>
-      {/* Section A (Navbar and Hero section)  */}
-      <section className={` ${styles.section}  `}>
-        {!showPage && <CarAnimation onComplete={handleAnimationComplete} />}
-        {!showPage && <TextAnimation onComplete={handleAnimationComplete} />}
-        {showPage && <NavBar />}
-        {showPage && <Hero />}
+      <section className={`${styles.section}`}>
+    
+        <NavBar />
+        <Hero />
       </section>
     </>
   );
 };
 
-export default Home
+export default Home;
